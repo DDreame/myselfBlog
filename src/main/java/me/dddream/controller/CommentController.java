311cdeabc29c9636da561b24ee82c9c523d50474
DDDreame <shadowxiaoruantian@gmail.com>
2023-06-21 20:21:51 +0800

    feat[common.result]: 参考其他项目设计返回值，统一对外返回对象。

--- src/main/java/me/dddream/controller/CommentController.java
+++ src/main/java/me/dddream/controller/CommentController.java
@@ -1,6 +1,9 @@
 package me.dddream.controller;
 
 import com.minis.web.RequestMapping;
+import com.minis.web.ResponseBody;
+import me.dddream.common.result.Result;
+import me.dddream.common.result.Results;
 
 /***
  * @description : 评论控制器
@@ -10,21 +13,24 @@ import com.minis.web.RequestMapping;
 public class CommentController {
 
     @RequestMapping("/api/v1/comment/add")
-    public String addComment() {
+    @ResponseBody
+    public Result<String> addComment() {
         //todo 添加评论
-        System.out.println("add comment is not completed");
-        return "add comment is not completed";
+        System.out.println("add comment is not completed!!");
+        return Results.success("add comment is not completed");
     }
     @RequestMapping("/api/v1/comment/del")
-    public String delComment() {
+    @ResponseBody
+    public Result<String> delComment() {
         //todo 删除评论
         System.out.println("del comment is not completed");
-        return "del comment is not completed";
+        return Results.success("del comment is not completed");
     }
     @RequestMapping("/api/v1/comment/edit")
-    public String editComment() {
+    @ResponseBody
+    public Result<String> editComment() {
         //todo 编辑评论
         System.out.println("edit comment is not completed");
-        return "edit comment is not completed";
+        return Results.success("edit comment is not completed");
     }
 }
---
+++ src/test/java/Test.java
@@ -0,0 +1,11 @@
+/***
+ * @description : Todo
+ * @author : DDDreame
+ * @date : 2023/6/21 20:12 
+ */
+public class Test {
+    public static void main(String[] args) {
+        Test x = null;
+        System.out.println(x.toString());
+    }
+}
